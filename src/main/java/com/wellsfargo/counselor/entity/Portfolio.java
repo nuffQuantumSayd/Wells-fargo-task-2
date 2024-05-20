d758f7d09e67190727dbe63c857c81b6298157d7
aaron <aaronkornish.ak@gmail.com>
2024-05-19 17:13:41 -0700

    Added portfolio properties

--- src/main/java/com/wellsfargo/counselor/entity/Portfolio.java
+++ src/main/java/com/wellsfargo/counselor/entity/Portfolio.java
@@ -1,4 +1,23 @@
 package com.wellsfargo.counselor.entity;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import org.springframework.data.annotation.Id;
+
+import java.util.Date;
+
+@Entity
 public class Portfolio {
+
+    @Id
+    @GeneratedValue()
+    private long portfolioId;
+
+    @Column(nullable = false)
+    private long clientId;
+
+    @Column(nullable = false)
+    private Date creationDate;
+
 }
